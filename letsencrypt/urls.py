6d197a91ac68786a9454e6f4b8691f06e01c42e0
Steve Garton <steve.garton@oraclefinance.co.uk>
2018-06-25 14:41:43 +0100

    Updating for Django2

--- letsencrypt/urls.py
+++ letsencrypt/urls.py
@@ -13,12 +13,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
-
 urlpatterns = [
-    url(r'^acme-challenge/(?P<acme_data>.+)$', views.detail, name='detail'),
+    re_path('acme-challenge/(?P<acme_data>.+)$', views.detail, name='detail'),
 ]
